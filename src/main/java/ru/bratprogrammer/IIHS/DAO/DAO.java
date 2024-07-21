0e6a7c507286f9b64fa1de12435e3763988e4ec4
BratProgrammer <travckin.4@yandex.ru>
2024-07-22 00:16:02 +0300

    small refactoring

--- src/main/java/ru/bratprogrammer/IIHS/DAO/DAO.java
+++ src/main/java/ru/bratprogrammer/IIHS/DAO/DAO.java
@@ -1,15 +1,12 @@
 package ru.bratprogrammer.IIHS.DAO;
 
-import org.springframework.transaction.annotation.Transactional;
-import ru.bratprogrammer.IIHS.Entities.Car;
-
 import java.util.List;
 
 public interface DAO<T> {
-    public void create(T t);
-    public void update(int id, T t);
-    public T getById(int id);
-    public List<T> getAll();
-    public void delete(T t);
-    public void deleteById(int id);
+    void create(T t);
+    void update(int id, T t);
+    T getById(int id);
+    List<T> getAll();
+    void delete(T t);
+    void deleteById(int id);
 }
